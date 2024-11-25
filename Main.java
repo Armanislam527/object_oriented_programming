c6d30137bda8f8e0008ff83baff714ff3f985963
arman islam <armanislam527@gmail.com>
2024-11-25 23:18:57 +0600

    compiltion

--- Main.java
+++ Main.java
@@ -1,39 +1,24 @@
-// Variable Declaration
-String name = "Alice";
-int age = 30;
-
-// Control Structures
-if (age >= 18) {
-    System.out.println("Adult");
-} else {
-    System.out.println("Minor");
-}
-
-// Function Definition
-public static String greet(String name) {
-    return "Hello, " + name + "!";
-
-}
-
-// Class Definition
-class Animal {
-
-    String name;
-
-    public Animal(String name) {
-        this.name = name;
-    }
-
-    public String speak() {
-        return "I am an animal.";
-    }
-}
-
-// Main Method and Object Creation
 public class Main {
 
+    // Function Definition
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+
     public static void main(String[] args) {
-        Animal dog = new Animal("Dog");
-        System.out.println(dog.speak());  // Output: I am an animal.
+        // Variable Declaration
+        String name = "Alice";
+        int age = 30;
+
+        // Control Structures
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Minor");
+        }
+
+        // Using the greet function
+        System.out.println(greet(name));
     }
 }
+
